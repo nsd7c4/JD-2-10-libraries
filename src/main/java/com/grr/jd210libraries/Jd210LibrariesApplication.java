1383e08a41d12ab125567f8a50ee92a974d76a81
rashidgafurov <nsd7c4@gmail.com>
2022-07-01 08:31:43 -0400

    Initial commit

---
+++ src/main/java/com/grr/jd210libraries/Jd210LibrariesApplication.java
@@ -0,0 +1,13 @@
+package com.grr.jd210libraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jd210LibrariesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jd210LibrariesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grr/jd210libraries/Jd210LibrariesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grr.jd210libraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jd210LibrariesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
